d4c19dc6b985b94881783889fa59e2358276df6e
Achmad Iqbal Priranda <79825994+balpr@users.noreply.github.com>
2022-04-18 14:21:56 +0700

    Add files via upload

---
+++ Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver = false;
+    public float topBound = 15;
+    public float floatForce;
+    public GameObject reloadText;
+
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+    private Vector3 lowPos = new Vector3(-3, 2, 0);
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        reloadText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+
+        if(transform.position.y > topBound)
+        {
+        	transform.position = new Vector3(transform.position.x, topBound, transform.position.z);
+        }
+
+        if(gameOver)
+        {
+        	reloadText.SetActive(true);
+        }
+
+        if(Input.GetKey(KeyCode.R) && gameOver)
+        {
+        	SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+        	playerAudio.PlayOneShot(bounceSound, 1.0f);
+        	gameObject.transform.position = lowPos;
+        }
+
+    }
+
+}
